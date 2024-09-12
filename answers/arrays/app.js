39d413fe612baf61583414dc8584d12261fdb8e0
wajahat-minhal <wajahatminhal200@gmail.com>
2024-09-12 19:08:13 +0500

    arrays assignment

---
+++ answers/arrays/app.js
@@ -0,0 +1,52 @@
+// Declare an empty array using JS literal notation to store            answer of Q-1
+// student names in future.
+
+// var students_Names = [];
+// console.log(typeof(students_Names));
+
+
+// Declare an empty array using JS object notation to store             answer of Q-2
+// student names in future.
+
+// var students_Names = new Array();
+// console.log(typeof(students_Names));
+
+
+
+// Declare and initialize a strings array.              answer of Q-3
+
+// var arr = ["wajahat", "minhal"]
+// console.log(arr)
+
+
+// Declare and initialize a numbers array.              answer of Q-4
+
+// var num = [10, 7, 4, 5]
+// console.log(num)
+
+
+// Declare and initialize a boolean array.              answer of Q-5
+
+// var bool = [true, false, false, true]
+// console.log(typeof(bool));
+
+
+// Declare and initialize a mixed array.                answer of Q-6
+
+// var mixArr = ["wajahat", 20, true]
+// console.log(mixArr)
+
+// Declare and Initialize an array and store available mobile           answer of Q-7
+// networks in Pakistan.
+
+// var networks = ["jazz", "ufone", "zong", "telenor", "onic"]
+// console.log(networks)
+
+// Declare and Initialize an array and store available education            answer of Q-8
+// qualifications in Pakistan (e.g. SSC, HSC, BCS, BS, BCOM, MS, 
+// M. Phil., PhD). Show the listed qualifications in your browser 
+// like:
+
+
+// var education = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"]
+
